7de9387c092eb45bdb04d6626395fd0193fe2d9f
JungYoungsu <terun707@naver.com>
2019-02-19 15:28:07 +0900

    (package name change)

--- src/main/java/com/multi/contactsapp/dao/ContactRepository.java
+++ src/main/java/com/multi/contactsapp/dao/ContactRepository.java
@@ -3,7 +3,7 @@ package com.multi.contactsapp.dao;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import com.multicampus.contactsapp.domain.Contact;
+import com.multi.contactsapp.domain.Contact;
 
 @RepositoryRestResource
 public interface ContactRepository extends PagingAndSortingRepository<Contact, Integer> {
